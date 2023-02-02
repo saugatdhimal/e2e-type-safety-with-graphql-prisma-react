c6124fb8d6d7546d7cb9bc7244025880b7722784
saugatdhimal <dhimalsaugat77@gmail.com>
2023-02-03 01:23:57 +0545

    add graphql api

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world")
+import {createYoga} from 'graphql-yoga'
+import {createServer} from 'node:http'
+
+import {schema} from './schema';
+
+
+const port = Number(process.env.API_PORT) || 4000
+
+// Create a Yoga instance with a GraphQL schema.
+const yoga = createYoga({schema})
+
+// Pass it into a server to hook into request handlers.
+const server = createServer(yoga)
+
+// Start the server and you're done!
+server.listen(port, () => {
+    console.info(`Server is running on http://localhost:${port}/graphql`)
+})
